64b2037875ebb1dc051785d75d8f0a4e7856ece3
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-02-13 22:56:11 -0500

    added event listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'phoenix-wc-wiki-ngx';
+  title = "phoenix-wc-wiki-ngx";
+
+  sendCustomEvent() {
+    const data = {
+      name: "DataSentThoughtCustomEvent"
+    };
+    const event = new CustomEvent("phoenix-wc-wiki-ngx-ce-data-sent-test", {
+      detail: data
+    });
+    window.dispatchEvent(event);
+  }
 }
